2d87f206f618b6665c2c0dd543f3cfdbdc23d6be
Lu <lucimar7324@hotmail.com>
2022-06-24 09:15:09 -0300

    Teste JSON

---
+++ index.html/jjjs.js
@@ -0,0 +1,159 @@
+var details = [
+    {
+        "id": 1,
+         "nacao": "Cavaleirode Rohan",
+         "casa": "Abismo de Helm",
+         "descricao": "Durante a XIII século de Terceira Idade (TA), os governantes de Gondor formaram uma aliança estreita com os habitantes do território do norte deRhovanion, um pai de três pessoas Casas do Edain da Primeira Era. Estes homens foram dizimados pela  Grande  Peste  de  1636  TA,  e  ainda  mais  pelas  invasões  dos  carrinhos  de Pessoas-de-no final do XX século. Após essas invasões, alguns desses homens, liderados por um certo Frumgar, migraram para o vale superior do Anduin em 1977 TA. No local, eles entraram em conflito com os Anões sobre o compartilhamento do tesouro do dragão Scatha, morto por Fram, filho de Frumgar. Esses homens, que tomaram o nome de Éothéod, viveram por vários séculos ao redor das nascentes do Anduin, mas se tornaram cada vez mais numerosos e se sentiam confinados em suas terras.  Apesar  do  afastamento,  eles  mantiveram  laços  com  Gondor,  e  quando  o Administrador Cirion se viu atacado por hordas de Orientais e Orcs, em 2510 AT, ele chamou Eorl, o Jovem, o novo Senhor dos Éothéod. Ele chegou inesperadamente e  venceu  a  batalha  decisiva  dos  Campos  de  Celebrant.",
+         "personagens_Saga": ["Theoden", "Theodred", "Deorwine"],
+         "imagem": "https://i.pinimg.com/564x/4f/7b/4f/4f7b4fff4ec69d9c47146dafd7d6374c.jpg"
+    },
+    
+    {
+        "id": 2,
+        "nacao": "Anao",
+        "casa": "Povo de Durin",
+        "descricao": "Gimli, filho de Glóin, era um anão guerreiro respeitado na Terra-Média. Ele era um membro da Sociedade do Anel, e foi o único dos anões que lutou com os elfos na guerra contra Sauron no final da Terceira Era.",
+        "personagens_saga": ["Balin", "Bombur", "Durin, o Imortal", "Dwalin"],
+        "imagem": "http://s2.glbimg.com/CDE5hCxFc_kLuZOIqNRnHvUoWWA=/600x400/smart/e.glbimg.com/og/ed/f/original/2017/11/16/gimli_-_fotr.png"
+    },
+    
+    {
+        "id": 3,
+        "nacao": "Elfo da Floresta",
+        "casa": "Floresta das trevas",
+        "descricao": "Os Elfos são descritos como altos e belos, parecidos com os Valar (espécie de anjos), só que menores em estatura e poder, e são imortais, pelos menos enquanto o Mundo, chamado Arda, existir. Não envelhecem nem adoecem, e se forem mortalmente feridos ou se sofrerem um grande desgosto seu corpo morre, mas seu espírito sobrevive sendo então enviado para as Mansões de Mandos onde permanece até poder reencarnar, em um corpo idêntico e com as mesmas lembranças. Um direito que os elfos têm é o de ir, se assim desejarem, para Valinor, no continente sagrado de Aman, destino esse vedado aos mortais.",
+        "personagens_saga": ["Balin", "Bombur", "Durin, o Imortal", "Dwalin"],
+        "imagem": "https://i.pinimg.com/originals/da/ef/69/daef690a5c52dfbea351f8cc69257226.png"
+    },
+    
+    {
+        "id": 4,
+        "nacao": "Hobbit",
+        "casa": "O Condado",
+        "descricao": "Os  hobbits pés-peludos são o tipo mais comum de hobbit. São morenos, preferem planícies e regiões banhadas por rios. Os cascalvas tem a pele e o cabelo mais claros, são mais altos e mais esguios que os outros tipos de hobbit, preferem bosques e florestas para morar além de serem descendentes de elfos. Outros hobbits dizem que estes têm 'sangue de fada', por conta de sua ascendência. Os grados são tipos de hobbit mais robustos e frequentemente cultivam barbas. São mais próximos aos  anões,  e  Sméagol  costumava  ser  um  cascalva  antes  de  ser  corrompido  pelo anel",
+        "personagens_saga": ["Bilbo Bolseiro", "Samwise Gamgee", "Frodo Bolseiro", "Peregrin Tok", "Rosinha villa"],
+        "imagem": "https://cinemacomrapadura.com.br/imagens/2012/01/Hobbit.jpeg"
+    },
+    
+    {
+        "id": 5,
+        "nacao": "Elfo de Lothlórien",
+        "casa": "Floresta das trevas",
+        "descricao": "O nome Lothlórien é uma abreviação do nome Laurelindórenan. Além de Lothlórien, no entanto, o reino pode ainda ser chamado apenas de Lórien, já que Lothlórien significa Lórien da Flor. No entanto, Lórien era na verdade o reino do vala Irmo, que mora em Aman, as Terras Imortais.",
+        "personagens_saga": ["Narya", " Nenya", "Wilya"],
+        "imagem": "https://www.gamersdecide.com/sites/default/files/authors/u140175/elrond.jpg"
+    },
+    
+    {
+        "id": 6,
+        "nacao": "Orc Snaga",
+        "casa": "Cirith Ungol",
+        "descricao": "Snaga era o segundo no comando de Grishnakh. Snaga e Grishnakh encontraram o bando de Ugluk e mandaram Ugluk levar os Hobbits para o Grande Rio onde um Nazgul esperava por eles. ",
+        "personagens_saga": ["Melhor", "Sauron", "Valor", "Nazgul"],
+        "imagem": "http://www.codinomeleitora.com.br/wp-content/uploads/orc-lord-of-the-ring.jpg"
+    },
+    
+    {
+        "id": 7,
+        "nacao": "Uruk-hai",
+        "casa": "Portao Negro",
+        "descricao": "Os Uruk-hai eram mais usados como soldados do que as outras linhagens porque, ao contrario dos outros orcs, eram quase tao altos quanto os homens, tinham pernas longas e retas. ",
+        "personagens_saga": ["Gorbag", "Shagrat", "Ugluk", "Azog", "Bolg"],
+        "imagem": "https://http2.mlstatic.com/senhor-dos-aneis-D_NQ_NP_21375-MLB20209598666_122014-F.jpg"
+    },
+    
+    {
+        "id": 8,
+        "nacao": "Guerreiro de Gondor",
+        "casa": "Minas Tirith",
+        "descricao": "Gimli, filho de Glóin, era um anão guerreiro respeitado na Terra-Média. Ele era um membro da Sociedade do Anel, e foi o único dos anões que lutou com os elfos na guerra contra Sauron no final da Terceira Era.",
+        "personagens_saga": ["Aragom", "Boromir", "Aldamir"],
+        "imagem": "http://3.bp.blogspot.com/_WvJ8Bcw1ZPc/TTqK9h8jz0I/AAAAAAAAAAw/ZMFKfE34BfA/s1600/Faramir.jpg"
+    }
+]
+
+function next_lore(next) {
+    let imagem;
+    var a = next;
+    var show = "<table border = '1' style = width: 100%;>"+"<tr>" + "<th>" + "<strong>" + "id" + "</strong>" + "</th>" + "<th>" + "<strong>" + "Nacao" + "</strong>" + "</th>" + "<th>" + "<strong>" + "Casa" + "</strong>" +"</th>" + "<th>" + "<strong>" + "Descricao" + "</strong>" + "</th>" + "<th>" + "<strong>" + "personagem_saga" + "</strong>" + "</th>" + "<th>" + "<strong>" + "imagem" + "</strong>" + "</th>" + "</tr>" + "<tr>" + "<td>" + "<strong>" + details[a].id + "</strong>" + "</td>" +"<td>" + "<strong>" + details[a].nacao + "</strong>" + "</td>"  + "<td>" + "<strong>" + details[a].casa + "</strong>" + "</td>" + "<td>" + "<strong>" + details[a].descricao + "</strong>" +"</td>" + "<td>" + "<strong>" + details[a].personagens_saga + "</strong>" + "</td>" +"<td>" + "<strong>" + details[a].imagem + "</strong>" + "</td>" + "<td>" + "</td>" + "</tr>" + "</table>" + "<br><br>";
+ 
+    var element = document.getElementById("dynamic").innerHTML = show;
+ 
+    imagem = document.createElement('img');
+    imagem.setAttribute("src",details[a].imagem);
+ 
+    if(a == 7) {
+       imagem.setAttribute("width","400");
+       imagem.setAttribute("height","400");
+       imagem.style.display = "flex";
+       imagem.style.margin = "0,auto";
+    } else {
+       imagem.setAttribute("width","auto");
+       imagem.setAttribute("height","auto");
+       imagem.style.display = "flex";
+       imagem.style.margin = "0,auto";
+    }
+ 
+    document.getElementById("dynamic").append(imagem);
+ 
+    details_cont++;
+ 
+    return details_cont;
+    
+ }
+
+ function previous_lore(prev) {
+    var a = prev - 1;
+    var show = "<table border = '1' style = width: 100%;>"+"<tr>" + "<th>" + "<strong>" + "id" + "</strong>" + "</th>" + "<th>" + "<strong>" + "Nacao" + "</strong>" + "</th>" + "<th>" + "<strong>" + "Casa" + "</strong>" +"</th>" + "<th>" + "<strong>" + "Descricao" + "</strong>" + "</th>" + "<th>" + "<strong>" + "personagem_saga" + "</strong>" + "</th>" + "<th>" + "<strong>" + "imagem" + "</strong>" + "</th>" + "</tr>" + "<tr>" + "<td>" + "<strong>" + details[a].id + "</strong>" + "</td>" +"<td>" + "<strong>" + details[a].nacao + "</strong>" + "</td>"  + "<td>" + "<strong>" + details[a].casa + "</strong>" + "</td>" + "<td>" + "<strong>" + details[a].descricao + "</strong>" +"</td>" + "<td>" + "<strong>" + details[a].personagens_saga + "</strong>" + "</td>" +"<td>" + "<strong>" + details[a].imagem + "</strong>" + "</td>" + "<td>" + "</td>" + "</tr>" + "</table>" + "<br><br>";
+ 
+    var element = document.getElementById("dynamic").innerHTML = show;
+ 
+    let imagem = document.createElement('img');
+    imagem.setAttribute("src",details[a].imagem);
+ 
+    if(a == 7) {
+       imagem.setAttribute("width","400");
+       imagem.setAttribute("height","400");
+       imagem.style.display = "flex";
+       imagem.style.margin = "0,auto";
+    } else {
+       imagem.setAttribute("width","auto");
+       imagem.setAttribute("height","auto");
+       imagem.style.display = "flex";
+       imagem.style.margin = "0,auto";
+    }
+ 
+ 
+    document.getElementById("dynamic").append(imagem);
+ 
+    details_cont = a;
+ 
+    return details_cont;
+ }
+
+ /*funcao formularios dinamicos */
+
+ function add_btn(a) {
+    if(cont < 8) {
+        var form = document.querySelector('.form_container').cloneNode(true);
+        document.querySelector('.form_container').after(form);
+    } else {
+        return 0;
+    }
+
+    return (++cont);
+}
+
+function rmv_btn() {
+    var form = document.querySelector('.form_container');
+    if(cont > 1) {
+        form.remove();
+        --cont;
+    } else return 0;
+}
+/* variaveis declarada.*/
+
+let details_cont = 0;
+let cont = 1;
+
+
